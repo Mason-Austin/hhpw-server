83fcad0520f0293a7a2cabb90c3092439e32f98e
Mason Austin <mason47austin@gmail.com>
2024-01-08 19:16:25 -0600

    completed revenue model

---
+++ hhpwapi/models/revenue.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .order import Order
+
+class Revenue(models.Model):
+  
+    total = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+    payment_type = models.CharField(max_length=50)
+    tip = models.FloatField()
+    order_type = models.CharField(max_length=50)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
