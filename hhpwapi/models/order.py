c364b791b133fdb9d14589b09e3670bfbbdac06a
Mason Austin <mason47austin@gmail.com>
2024-01-08 19:13:52 -0600

    completed order model

---
+++ hhpwapi/models/order.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .user import User
+
+class Order(models.Model):
+
+    name = models.CharField(max_length=50)
+    open = models.BooleanField(default=False)
+    customer_phone = models.IntegerField()
+    customer_email = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
