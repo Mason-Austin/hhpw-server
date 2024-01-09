a933573352aa27f50b66aeaedab7308879a30436
Mason Austin <mason47austin@gmail.com>
2024-01-08 20:45:36 -0600

    added the check user

--- hhpwapi/views/auth.py
+++ hhpwapi/views/auth.py
@@ -1,6 +1,6 @@
-from hhpwapi.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from hhpwapi.models import User
 
 @api_view(['POST'])
 def register_user(request):
@@ -23,3 +23,29 @@ def register_user(request):
         'name': user.name
     }
     return Response(data)
+
+@api_view(['POST'])
+def check_user(request):
+    '''Checks to see if User has Associated obj
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+    uid = request.data['uid']
+
+    # Use the built-in authenticate method to verify
+    # authenticate returns the user object or None if no user is found
+    user = User.objects.filter(uid=uid).first()
+
+    # If authentication was successful, respond with their token
+    if user is not None:
+        data = {
+            'id': user.id,
+            'uid': user.uid,
+            'name': user.name
+        }
+        return Response(data)
+    else:
+        # Bad login details were provided. So we can't log the user in.
+        data = { 'valid': False }
+        return Response(data)
