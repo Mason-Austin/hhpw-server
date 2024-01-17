a9061501aaa30cee9b64044e718c98b2c63b691a
Mason Austin <mason47austin@gmail.com>
2024-01-16 18:29:17 -0600

    created the retrieve for items, and the seriializer for items

---
+++ hhpwapi/views/items.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from ..models import Item
+
+
+class ItemView(ViewSet):
+    """hhpw item view"""
+    def retrieve(self, request, pk):
+        """Handle GET requests for a single item type
+        
+        returns:
+        Response -- JSON Serialzied item"""
+        try:
+            item = Item.objects.get(pk=pk)
+            serializer = ItemSerializer(item)
+            return Response(serializer.data)
+        except Item.DoesNotExist as ex:
+          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+class ItemSerializer(serializers.ModelSerializer):
+      """JSON serializer for Item"""
+      class Meta:
+          model = Item
+          fields = ("id", "name", "price")
